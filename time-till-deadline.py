6049dbb8f2673eefe0f5d710474e7625314dc61f
Iheb <Iheb.mansouri@etudiant-isi.utm.tn>
2023-07-22 16:00:17 +0100

    second commit

--- time-till-deadline.py
+++ time-till-deadline.py
@@ -1,7 +1,3 @@
-#"time-till-deadline" is a Python script designed to calculate and display the time remaining until a specified deadline. 
-#Users can input a target date and time, and the script will calculate the duration between the current date and time and the provided deadline. 
-#The output will be in a human-readable format, showing the remaining time in days and/or hours until the deadline is reached.
-
 import datetime
 
 
